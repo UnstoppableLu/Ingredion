6b821df5db347512cd371d98b5c3435d13e7bd0f
kaimingc <kchmi5531@gmail.com>
2025-10-28 21:27:58 -0400

    updates

--- app/extractor/test.py
+++ app/extractor/test.py
@@ -1,4 +1,4 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyDU72NOX7mDBqXFUXv-X8oncNXZF0RCzGs")
+genai.configure(api_key="api-key")
 for m in genai.list_models():
     print(m.name)
